564935437605137bcf94b525652e8c3471a2c5cf
Jéssica Hatsumi Isibasi <116028793+jessicahaty@users.noreply.github.com>
2023-07-21 00:17:06 -0300

    Add files via upload

---
+++ Concatena strings/main.js
@@ -0,0 +1,9 @@
+ function concatenar() {
+    var texto1 = document.getElementById('texto-1').value;
+    var texto2 = document.getElementById('texto-2').value;
+
+    var resultado = texto1 + ' ' + texto2;
+
+    document.write(resultado);
+ }
+
