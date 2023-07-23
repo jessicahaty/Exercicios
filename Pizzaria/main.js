3736582e031867f02e8c5628aa6c0b593b3fbc9e
Jéssica Hatsumi Isibasi <116028793+jessicahaty@users.noreply.github.com>
2023-07-23 20:16:54 -0300

    Add files via upload

---
+++ Pizzaria/main.js
@@ -0,0 +1,13 @@
+function calcular() {
+
+    var custo = document.getElementById('custo').value
+    var valorVendido = document.getElementById('valorVendido').value
+    var nomePizza = document.getElementById('nomePizza').value
+
+    var imposto = parseFloat((valorVendido*0.089))
+
+    var lucro = (valorVendido - custo - imposto)
+   
+
+    document.getElementById('resposta').innerHTML = "Para a Pizaa " + nomePizza + " o lucro liquido é de R$" + lucro + "e o imposto é de "+ imposto; 
+}
